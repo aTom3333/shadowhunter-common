9683ddf9201b68e8e80d126145d93df7b65af173
Thomas Ferrand <a.tom.hic3@gmail.com>
2019-09-12 22:19:20 -0400

    fix oversight

--- Game/Board.ts
+++ Game/Board.ts
@@ -4,14 +4,14 @@ import {CharacterState} from "./CharacterState";
 export class Board {
     states: Array<CharacterState>;
 
-    nextOf(index) {
+    nextOf(index: number) {
         index += 1;
         if(index >= this.states.length)
             index = 0;
         return index;
     }
 
-    previousOf(index) {
+    previousOf(index: number) {
         index -= 1;
         if(index < 0)
             index = this.states.length-1;
